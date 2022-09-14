a22efb548794a7c4ac3cbec836f036bf67177aef
Alexis Nanna <alexis.nanna.fb@gmail.com>
2022-09-14 19:59:16 -0400

    first knowledge check

---
+++ data_check.py
@@ -0,0 +1,11 @@
+print("Hello World")
+
+fruits = ['banana', 'orange', 'apple', 'grape']
+print(fruits[2])
+
+good_movies = {1: 'Inception', 2: 'Titanic'}
+print(good_movies[1])
+
+color_tuple = ('red', 'blue', 'green', 'yellow')
+print(color_tuple[3])
+
